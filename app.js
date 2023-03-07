bcb70069494bada5e7a62e6071286796ba0bde6f
Daniel Schaefer <dhs@frame.work>
2023-03-08 05:47:49 +0800

    Allow connecting via webserial
    And directly draw on the matrix.
    
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
+const BRIGHTNESS_CMD = 0x00;
+const PATTERN_CMD = 0x01;
+const BOOTLOADER_CMD = 0x02;
+const SLEEP_CMD = 0x03;
+const ANIMATE_CMD = 0x04;
+const PANIC_CMD = 0x05;
+const DRAW_CMD = 0x06;
+const STAGE_GREY_COL_CMD = 0x07;
+const DrawGreyColBuffer = 0x08;
+const SetText = 0x09;
+const StartGame = 0x10;
+const GameControl = 0x11;
+const GameStatus = 0x12;
+const SetColor = 0x13;
+const DisplayOn = 0x14;
+const InvertScreen = 0x15;
+const SetPixelColumn = 0x16;
+const FlushFramebuffer = 0x17;
+const VERSION_CMD = 0x20;
+
 var matrix;
 var $table;
 var rowMajor = false;
 var msbendian = false;
+let port = null;
 
 $(function() {
   	matrix = createArray(34, 9);
@@ -26,7 +47,9 @@ function updateTable() {
 
 function initOptions() {
 	$('#clearButton').click(function() { matrix = createArray(matrix.length,matrix[0].length); updateTable(); $('#_output').hide(); });
+	$('#connectButton').click(connectSerial);
 	$('#generateButton').click(updateCode);
+	$('#sendButton').click(sendToDisplay);
 
 	 $('#widthDropDiv li a').click(function () {
 	 	var width = parseInt($(this).html());
@@ -71,6 +94,60 @@ function updateSummary() {
 	else summary += "little endian.";
 
 	$('#_summary').html(summary);
+}
+
+async function command(port, id, params) {
+  const writer = port.writable.getWriter();
+
+  let bytes = [0x32, 0xAC];
+  bytes = bytes.concat([id]);
+  bytes = bytes.concat(params);
+  console.log('Params:', bytes);
+
+  const data = new Uint8Array(bytes);
+  await writer.write(data);
+
+  // Allow the serial port to be closed later.
+  writer.releaseLock();
+}
+
+function foo() {
+	const width = matrix[0].length;
+	const height = matrix.length;
+
+  let vals = new Array(39).fill(0);
+
+  for (let col = 0; col < width; col++) {
+    for (let row = 0; row < height; row++) {
+      const cell = matrix[row][col];
+      if (cell == 1) {
+        const i = col + row * width;
+        vals[Math.trunc(i/8)] |= 1 << i % 8;
+      }
+    }
+  }
+  return vals;
+}
+
+async function sendToDisplay() {
+  let vals = foo();
+  console.log("Send bytes:", vals);
+  //command(port, SLEEP_CMD, [1]);
+  command(port, DRAW_CMD, vals);
+}
+
+async function connectSerial() {
+  port = await navigator.serial.requestPort();
+
+  const { usbProductId, usbVendorId } = port.getInfo();
+  console.log(`Selected`, port);
+  console.log(`VID:PID ${usbVendorId}:${usbProductId}`);
+
+  if (port.readable === null || port.writeable === null) {
+    console.log("Opening port");
+    await port.open({ baudRate: 115200 });
+  }
+
 }
 
 function updateCode() {
@@ -124,6 +201,10 @@ function toggle(e) {
 		matrix[x][y] = 0;
 		$(this).removeClass('on');	
 	}
+
+  if (port) {
+    sendToDisplay();
+  }
 
 	return false;
 }
